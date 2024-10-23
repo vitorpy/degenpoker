9bdf2b4d5c7c1b2e4c81805f3378064c68440ebc
Vitor Py Braga <12871+vitorpy@users.noreply.github.com>
2024-10-23 17:04:10 +0200

    feat: add magic block example contract

--- anchor/programs/degenpoker/src/lib.rs
+++ anchor/programs/degenpoker/src/lib.rs
@@ -1,70 +1,153 @@
-#![allow(clippy::result_large_err)]
-
 use anchor_lang::prelude::*;
+use ephemeral_rollups_sdk::anchor::{commit, delegate, DelegationProgram, MagicProgram};
+use ephemeral_rollups_sdk::cpi::delegate_account;
+use ephemeral_rollups_sdk::ephem::{commit_accounts, commit_and_undelegate_accounts};
 
-declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
+declare_id!("4LubQAXySSXWDY8CL6pirr2UJYedPSxnmvVYiXupN81y");
 
+pub const TEST_PDA_SEED: &[u8] = b"test-pda";
+
+#[delegate]
 #[program]
-pub mod degenpoker {
+pub mod anchor_counter {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseDegenpoker>) -> Result<()> {
-    Ok(())
-  }
+    /// Initialize the counter.
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.degenpoker.count = ctx.accounts.degenpoker.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+    /// Increment the counter.
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        if counter.count > 1000 {
+            counter.count = 0;
+        }
+        Ok(())
+    }
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.degenpoker.count = ctx.accounts.degenpoker.count.checked_add(1).unwrap();
-    Ok(())
-  }
+    /// Delegate the account to the delegation program
+    pub fn delegate(ctx: Context<DelegateInput>) -> Result<()> {
+        let pda_seeds: &[&[u8]] = &[TEST_PDA_SEED];
 
-  pub fn initialize(_ctx: Context<InitializeDegenpoker>) -> Result<()> {
-    Ok(())
-  }
+        delegate_account(
+            &ctx.accounts.payer,
+            &ctx.accounts.pda,
+            &ctx.accounts.owner_program,
+            &ctx.accounts.buffer,
+            &ctx.accounts.delegation_record,
+            &ctx.accounts.delegation_metadata,
+            &ctx.accounts.delegation_program,
+            &ctx.accounts.system_program,
+            pda_seeds,
+            0,
+            u32::MAX,
+        )?;
+        Ok(())
+    }
+    /// Undelegate the account from the delegation program
+    pub fn undelegate(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        commit_and_undelegate_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
 
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.degenpoker.count = value.clone();
-    Ok(())
-  }
+    /// Increment the counter + manual commit the account in the ER.
+    pub fn increment_and_commit(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        commit_accounts(
+            &ctx.accounts.payer,
+            vec![
+                &ctx.accounts.counter.to_account_info(),
+            ],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
+
+    /// Increment the counter + manual commit the account in the ER.
+    pub fn increment_and_undelegate(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        // Serialize the Anchor counter account, commit and undelegate
+        counter.exit(&crate::ID)?;
+        commit_and_undelegate_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeDegenpoker<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Degenpoker::INIT_SPACE,
-  payer = payer
-  )]
-  pub degenpoker: Account<'info, Degenpoker>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseDegenpoker<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub degenpoker: Account<'info, Degenpoker>,
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8, seeds = [TEST_PDA_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub degenpoker: Account<'info, Degenpoker>,
+pub struct DelegateInput<'info> {
+    pub payer: Signer<'info>,
+    /// CHECK The pda to delegate
+    #[account(mut)]
+    pub pda: AccountInfo<'info>,
+    /// CHECK: The program that owns the pda
+    #[account(address = crate::id())]
+    pub owner_program: AccountInfo<'info>,
+    /// CHECK The temporary buffer account used during delegation
+    #[account(
+        mut, seeds = [ephemeral_rollups_sdk::consts::BUFFER, crate::id().as_ref()],
+        bump, seeds::program = delegation_program.key()
+    )]
+    pub buffer: AccountInfo<'info>,
+    /// CHECK: The delegation record account
+    #[account(
+        mut, seeds = [ephemeral_rollups_sdk::consts::DELEGATION_RECORD, pda.key().as_ref()],
+        bump, seeds::program = delegation_program.key()
+    )]
+    pub delegation_record: AccountInfo<'info>,
+    /// CHECK: The delegation metadata account
+    #[account(
+        mut, seeds = [ephemeral_rollups_sdk::consts::DELEGATION_METADATA, pda.key().as_ref()],
+        bump, seeds::program = delegation_program.key()
+    )]
+    pub delegation_metadata: AccountInfo<'info>,
+    pub delegation_program: Program<'info, DelegationProgram>,
+    pub system_program: Program<'info, System>,
+}
+
+/// Account for the increment instruction.
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, seeds = [TEST_PDA_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+}
+
+/// Account for the increment instruction + manual commit.
+#[commit]
+#[derive(Accounts)]
+pub struct IncrementAndCommit<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(mut, seeds = [TEST_PDA_SEED], bump)]
+    pub counter: Account<'info, Counter>,
 }
 
 #[account]
-#[derive(InitSpace)]
-pub struct Degenpoker {
-  count: u8,
+pub struct Counter {
+    pub count: u64,
 }
---
+++ anchor/target/types/anchor_counter.ts
@@ -0,0 +1,510 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/anchor_counter.json`.
+ */
+export type AnchorCounter = {
+  "address": "4LubQAXySSXWDY8CL6pirr2UJYedPSxnmvVYiXupN81y",
+  "metadata": {
+    "name": "anchorCounter",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "delegate",
+      "docs": [
+        "Delegate the account to the delegation program"
+      ],
+      "discriminator": [
+        90,
+        147,
+        75,
+        178,
+        85,
+        88,
+        4,
+        137
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "signer": true
+        },
+        {
+          "name": "pda",
+          "docs": [
+            "CHECK The pda to delegate"
+          ],
+          "writable": true
+        },
+        {
+          "name": "ownerProgram",
+          "address": "4LubQAXySSXWDY8CL6pirr2UJYedPSxnmvVYiXupN81y"
+        },
+        {
+          "name": "buffer",
+          "docs": [
+            "CHECK The temporary buffer account used during delegation"
+          ],
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  98,
+                  117,
+                  102,
+                  102,
+                  101,
+                  114
+                ]
+              },
+              {
+                "kind": "const",
+                "value": [
+                  49,
+                  172,
+                  144,
+                  55,
+                  200,
+                  40,
+                  142,
+                  33,
+                  212,
+                  32,
+                  17,
+                  25,
+                  214,
+                  192,
+                  0,
+                  65,
+                  166,
+                  10,
+                  251,
+                  204,
+                  85,
+                  123,
+                  76,
+                  56,
+                  226,
+                  111,
+                  26,
+                  56,
+                  67,
+                  156,
+                  112,
+                  236
+                ]
+              }
+            ],
+            "program": {
+              "kind": "account",
+              "path": "delegationProgram"
+            }
+          }
+        },
+        {
+          "name": "delegationRecord",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  100,
+                  101,
+                  108,
+                  101,
+                  103,
+                  97,
+                  116,
+                  105,
+                  111,
+                  110
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "pda"
+              }
+            ],
+            "program": {
+              "kind": "account",
+              "path": "delegationProgram"
+            }
+          }
+        },
+        {
+          "name": "delegationMetadata",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  100,
+                  101,
+                  108,
+                  101,
+                  103,
+                  97,
+                  116,
+                  105,
+                  111,
+                  110,
+                  45,
+                  109,
+                  101,
+                  116,
+                  97,
+                  100,
+                  97,
+                  116,
+                  97
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "pda"
+              }
+            ],
+            "program": {
+              "kind": "account",
+              "path": "delegationProgram"
+            }
+          }
+        },
+        {
+          "name": "delegationProgram",
+          "address": "DELeGGvXpWV2fqJUhqcF5ZSYMS4JTLjteaAMARRSaeSh"
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "increment",
+      "docs": [
+        "Increment the counter."
+      ],
+      "discriminator": [
+        11,
+        18,
+        104,
+        9,
+        104,
+        174,
+        59,
+        33
+      ],
+      "accounts": [
+        {
+          "name": "counter",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  116,
+                  101,
+                  115,
+                  116,
+                  45,
+                  112,
+                  100,
+                  97
+                ]
+              }
+            ]
+          }
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "incrementAndCommit",
+      "docs": [
+        "Increment the counter + manual commit the account in the ER."
+      ],
+      "discriminator": [
+        1,
+        27,
+        135,
+        183,
+        234,
+        124,
+        35,
+        207
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "counter",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  116,
+                  101,
+                  115,
+                  116,
+                  45,
+                  112,
+                  100,
+                  97
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "magicProgram",
+          "address": "Magic11111111111111111111111111111111111111"
+        },
+        {
+          "name": "magicContext",
+          "writable": true,
+          "address": "MagicContext1111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "incrementAndUndelegate",
+      "docs": [
+        "Increment the counter + manual commit the account in the ER."
+      ],
+      "discriminator": [
+        221,
+        12,
+        62,
+        164,
+        172,
+        218,
+        159,
+        199
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "counter",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  116,
+                  101,
+                  115,
+                  116,
+                  45,
+                  112,
+                  100,
+                  97
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "magicProgram",
+          "address": "Magic11111111111111111111111111111111111111"
+        },
+        {
+          "name": "magicContext",
+          "writable": true,
+          "address": "MagicContext1111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "initialize",
+      "docs": [
+        "Initialize the counter."
+      ],
+      "discriminator": [
+        175,
+        175,
+        109,
+        31,
+        13,
+        152,
+        155,
+        237
+      ],
+      "accounts": [
+        {
+          "name": "counter",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  116,
+                  101,
+                  115,
+                  116,
+                  45,
+                  112,
+                  100,
+                  97
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "user",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "processUndelegation",
+      "discriminator": [
+        196,
+        28,
+        41,
+        206,
+        48,
+        37,
+        51,
+        167
+      ],
+      "accounts": [
+        {
+          "name": "baseAccount",
+          "writable": true
+        },
+        {
+          "name": "buffer"
+        },
+        {
+          "name": "payer",
+          "writable": true
+        },
+        {
+          "name": "systemProgram"
+        }
+      ],
+      "args": [
+        {
+          "name": "accountSeeds",
+          "type": {
+            "vec": "bytes"
+          }
+        }
+      ]
+    },
+    {
+      "name": "undelegate",
+      "docs": [
+        "Undelegate the account from the delegation program"
+      ],
+      "discriminator": [
+        131,
+        148,
+        180,
+        198,
+        91,
+        104,
+        42,
+        238
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "counter",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  116,
+                  101,
+                  115,
+                  116,
+                  45,
+                  112,
+                  100,
+                  97
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "magicProgram",
+          "address": "Magic11111111111111111111111111111111111111"
+        },
+        {
+          "name": "magicContext",
+          "writable": true,
+          "address": "MagicContext1111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    }
+  ],
+  "accounts": [
+    {
+      "name": "counter",
+      "discriminator": [
+        255,
+        176,
+        4,
+        245,
+        188,
+        253,
+        124,
+        25
+      ]
+    }
+  ],
+  "types": [
+    {
+      "name": "counter",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "count",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ]
+};
